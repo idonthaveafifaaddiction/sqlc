c681700b23e83f5d8adc6eaa13e1fa7a652c903d
Jessica Bellon <10819486+JessTheBell@users.noreply.github.com>
2020-06-26 13:55:46 -0500

    test case

---
+++ internal/endtoend/testdata/mysql_dupl_name/query.sql
@@ -0,0 +1,12 @@
+/* name: SelectUserArg :many */
+SELECT  first_name from
+users where (sqlc.arg(id) = id OR sqlc.arg(id) = 0);
+
+/* name: SelectUserColon :many */
+SELECT  first_name from
+users where (:id = id OR :id = 0);
+
+
+/* name: SelectUserQuestion :many */
+SELECT  first_name from
+users where (? = id OR  ? = 0);
